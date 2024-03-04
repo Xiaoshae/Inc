39d169bff2fada42276eff0ade67482d2b53a995
Xiaoshae <xiaoshae@gmail.com>
2024-03-04 10:36:22 +0800

    添加inc和multe(含结构体)命名空间

--- inc.h
+++ inc.h
@@ -1,8 +1,42 @@
 ﻿//infoContainer
-#ifndef __INC__
+#ifndef __HEAD__INC__
 
-#define __INC__
+#define __HEAD__INC__
 
+#include <string>
 
+namespace __NSPE__INC__ {
+
+	namespace __NSPE__MULTE__ {
+
+		typedef size_t th;
+		typedef int8_t type;
+
+		using std::string;
+		typedef string u8;
+		typedef void* null;
+		typedef multeSpec** sn;
+		typedef size_t am; //amount
+
+		typedef multeSpec* multe;
+
+		struct multeSpec {
+
+			u8 Name;
+			null Fet;
+			sn Son;
+			am amSon;
+
+		};
+
+		const static th thmulte = sizeof(multeSpec);
+
+	}
+
+	namespace multe = __NSPE__MULTE__;
+
+}
+
+namespace inc = __NSPE__INC__;
 
 #endif // !__INC__
