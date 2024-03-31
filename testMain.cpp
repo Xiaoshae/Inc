163179d8e6dfb424de1be57e9d8138e3bba92b91
Xiaoshae <xiaoshae@gmail.com>
2024-03-31 20:20:06 +0800

    删掉所有内容，打算重构

--- testMain.cpp
+++ testMain.cpp
@@ -1,36 +1,10 @@
 ﻿#include <iostream>
 
-#include "inc.h"
-
-
-void incName::itf(void);
+using namespace std;
 
 int main(void) {
 
-	using std::cout;
-	using std::endl;
-
-	cout << "Welcome to infoContainer" << endl;
-
-
-	incName::itf();
+	cout << "Welcometo use inc Tools!" << endl;
 
 	return 0;
-}
-
-void incName::itf(void) {
-
-	using std::cout;
-	using std::endl;
-
-	inc a;
-	
-	cout << a.base << endl;
-
-
-
-	cout << a.getName() << endl;
-	a.setName("base");
-	cout << a.getName() << endl;
-	
 }
